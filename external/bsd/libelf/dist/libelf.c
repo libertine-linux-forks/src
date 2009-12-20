d9759ca9ab29a4187e2603e9035e4a7a3dc5126b
mrg <mrg>
2009-12-20 19:38:13 +0000

    PRIVATE_VERSION(arch) isn't ever used, so don't try to setup this member of _libelf{}.  ELF??_MACHDEP_ID may not be defined if ELFSIZE wasn't defined before including this file.
    this fixes a build error on mips platforms.

---
+++ external/bsd/libelf/dist/libelf.c
@@ -0,0 +1,76 @@
+/*	$NetBSD: libelf.c,v 1.4 2009/12/20 19:38:13 mrg Exp $	*/
+
+/*-
+ * Copyright (c) 2006 Joseph Koshy
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+/* __FBSDID("$FreeBSD: src/lib/libelf/libelf.c,v 1.1.10.1.2.1 2009/10/25 01:10:29 kensmith Exp $"); */
+__RCSID("$NetBSD: libelf.c,v 1.4 2009/12/20 19:38:13 mrg Exp $");
+
+#include <sys/param.h>
+
+#if defined(__FreeBSD__)
+#include <machine/elf.h>
+#endif /* __FreeBSD__ */
+#include <machine/endian.h>
+
+#include <libelf.h>
+
+#include "_libelf.h"
+
+#if defined(__NetBSD__)
+# if ARCH_ELFSIZE == 64
+#  define ELF_TARG_DATA		ELF64_MACHDEP_ENDIANNESS
+#  define ELF_TARG_CLASS	ELFCLASS64
+# else
+#  define ELF_TARG_DATA		ELF32_MACHDEP_ENDIANNESS
+#  define ELF_TARG_CLASS	ELFCLASS32
+# endif /* ARCH_ELFSIZE */
+#endif /* __NetBSD__ */
+
+struct _libelf_globals _libelf = {
+	.libelf_arch		= 0 /* unused - ELF_ARCH */,
+	.libelf_byteorder	= ELF_TARG_DATA,
+	.libelf_class		= ELF_TARG_CLASS,
+	.libelf_error		= 0,
+	.libelf_fillchar	= 0,
+	.libelf_version		= EV_NONE
+};
+
+
+#if defined(LIBELF_TEST_HOOKS)
+int
+_libelf_get_elf_class(Elf *elf)
+{
+	return elf->e_class;
+}
+
+void
+_libelf_set_elf_class(Elf *elf, int c)
+{
+	elf->e_class = c;
+}
+#endif	/* LIBELF_TEST_HOOKS */
