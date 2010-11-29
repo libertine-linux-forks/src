1e6df5cbba48426502f029f21743f374e1bc8fca
pooka <pooka>
2010-11-29 16:08:03 +0000

    Remove remaining panic()s from server-side code.  Also, allow to send an out-of-band error.  Make the client retry syscall requests if this error is EAGAIN, fail them otherwise.

---
+++ lib/librumpclient/rumpclient.c
@@ -0,0 +1,248 @@
+/*      $NetBSD: rumpclient.c,v 1.6 2010/11/29 16:08:03 pooka Exp $	*/
+
+/*
+ * Copyright (c) 2010 Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Client side routines for rump syscall proxy.
+ */
+
+#include <sys/cdefs.h>
+__RCSID("$NetBSD");
+
+#include <sys/param.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <rump/rumpclient.h>
+
+#include "sp_common.c"
+
+static struct spclient clispc;
+
+static int
+syscall_req(struct spclient *spc, int sysnum,
+	const void *data, size_t dlen, void **resp)
+{
+	struct rsp_hdr rhdr;
+	struct respwait rw;
+	int rv;
+
+	rhdr.rsp_len = sizeof(rhdr) + dlen;
+	rhdr.rsp_class = RUMPSP_REQ;
+	rhdr.rsp_type = RUMPSP_SYSCALL;
+	rhdr.rsp_sysnum = sysnum;
+
+	do {
+		putwait(spc, &rw, &rhdr);
+		rv = dosend(spc, &rhdr, sizeof(rhdr));
+		rv = dosend(spc, data, dlen);
+		if (rv) {
+			unputwait(spc, &rw);
+			return rv;
+		}
+
+		rv = waitresp(spc, &rw);
+	} while (rv == EAGAIN);
+
+	*resp = rw.rw_data;
+	return rv;
+}
+
+static int
+send_copyin_resp(struct spclient *spc, uint64_t reqno, void *data, size_t dlen,
+	int wantstr)
+{
+	struct rsp_hdr rhdr;
+	int rv;
+
+	if (wantstr)
+		dlen = MIN(dlen, strlen(data)+1);
+
+	rhdr.rsp_len = sizeof(rhdr) + dlen;
+	rhdr.rsp_reqno = reqno;
+	rhdr.rsp_class = RUMPSP_RESP;
+	rhdr.rsp_type = RUMPSP_COPYIN;
+	rhdr.rsp_sysnum = 0;
+
+	sendlock(spc);
+	rv = dosend(spc, &rhdr, sizeof(rhdr));
+	rv = dosend(spc, data, dlen);
+	sendunlock(spc);
+
+	return rv;
+}
+
+static int
+send_anonmmap_resp(struct spclient *spc, uint64_t reqno, void *addr)
+{
+	struct rsp_hdr rhdr;
+	int rv;
+
+	rhdr.rsp_len = sizeof(rhdr) + sizeof(addr);
+	rhdr.rsp_reqno = reqno;
+	rhdr.rsp_class = RUMPSP_RESP;
+	rhdr.rsp_type = RUMPSP_ANONMMAP;
+	rhdr.rsp_sysnum = 0;
+
+	sendlock(spc);
+	rv = dosend(spc, &rhdr, sizeof(rhdr));
+	rv = dosend(spc, &addr, sizeof(addr));
+	sendunlock(spc);
+
+	return rv;
+}
+
+int
+rumpclient_syscall(int sysnum, const void *data, size_t dlen,
+	register_t *retval)
+{
+	struct rsp_sysresp *resp;
+	void *rdata;
+	int rv;
+
+	DPRINTF(("rumpsp syscall_req: syscall %d with %p/%zu\n",
+	    sysnum, data, dlen));
+
+	rv = syscall_req(&clispc, sysnum, data, dlen, &rdata);
+	if (rv)
+		return rv;
+
+	resp = rdata;
+	DPRINTF(("rumpsp syscall_resp: syscall %d error %d, rv: %d/%d\n",
+	    sysnum, rv, resp->rsys_retval[0], resp->rsys_retval[1]));
+
+	memcpy(retval, &resp->rsys_retval, sizeof(resp->rsys_retval));
+	rv = resp->rsys_error;
+	free(rdata);
+
+	return rv;
+}
+
+static void
+handlereq(struct spclient *spc)
+{
+	struct rsp_copydata *copydata;
+	void *mapaddr;
+	size_t maplen;
+	int reqtype = spc->spc_hdr.rsp_type;
+
+	switch (reqtype) {
+	case RUMPSP_COPYIN:
+	case RUMPSP_COPYINSTR:
+		/*LINTED*/
+		copydata = (struct rsp_copydata *)spc->spc_buf;
+		DPRINTF(("rump_sp handlereq: copyin request: %p/%zu\n",
+		    copydata->rcp_addr, copydata->rcp_len));
+		send_copyin_resp(spc, spc->spc_hdr.rsp_reqno,
+		    copydata->rcp_addr, copydata->rcp_len,
+		    reqtype == RUMPSP_COPYINSTR);
+		break;
+	case RUMPSP_COPYOUT:
+	case RUMPSP_COPYOUTSTR:
+		/*LINTED*/
+		copydata = (struct rsp_copydata *)spc->spc_buf;
+		DPRINTF(("rump_sp handlereq: copyout request: %p/%zu\n",
+		    copydata->rcp_addr, copydata->rcp_len));
+		/*LINTED*/
+		memcpy(copydata->rcp_addr, copydata->rcp_data,
+		    copydata->rcp_len);
+		break;
+	case RUMPSP_ANONMMAP:
+		/*LINTED*/
+		maplen = *(size_t *)spc->spc_buf;
+		mapaddr = mmap(NULL, maplen, PROT_READ|PROT_WRITE,
+		    MAP_ANON, -1, 0);
+		if (mapaddr == MAP_FAILED)
+			mapaddr = NULL;
+		DPRINTF(("rump_sp handlereq: anonmmap: %p\n", mapaddr));
+		send_anonmmap_resp(spc, spc->spc_hdr.rsp_reqno, mapaddr);
+		break;
+	default:
+		printf("PANIC: INVALID TYPE\n");
+		abort();
+		break;
+	}
+
+	spcfreebuf(spc);
+}
+
+int
+rumpclient_init()
+{
+	struct sockaddr *sap;
+	char *p;
+	unsigned idx;
+	int error, s;
+
+	if ((p = getenv("RUMP_SP_CLIENT")) == NULL) {
+		errno = ENOENT;
+		return -1;
+	}
+
+	if ((error = parseurl(p, &sap, &idx, 0)) != 0) {
+		errno = error;
+		return -1;
+	}
+
+	s = socket(parsetab[idx].domain, SOCK_STREAM, 0);
+	if (s == -1)
+		return -1;
+
+	if (connect(s, sap, sap->sa_len) == -1) {
+		error = errno;
+		fprintf(stderr, "rump_sp: client connect failed\n");
+		errno = error;
+		return -1;
+	}
+
+	if ((error = parsetab[idx].connhook(s)) != 0) {
+		error = errno;
+		fprintf(stderr, "rump_sp: connect hook failed\n");
+		errno = error;
+		return -1;
+	}
+	clispc.spc_fd = s;
+	TAILQ_INIT(&clispc.spc_respwait);
+	pthread_mutex_init(&clispc.spc_mtx, NULL);
+	pthread_cond_init(&clispc.spc_cv, NULL);
+
+	return 0;
+}
---
+++ lib/librumpuser/sp_common.c
@@ -0,0 +1,661 @@
+/*      $NetBSD: sp_common.c,v 1.13 2010/11/29 16:08:03 pooka Exp $	*/
+
+/*
+ * Copyright (c) 2010 Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Common client/server sysproxy routines.  #included.
+ */
+
+#include <sys/cdefs.h>
+
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/queue.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <poll.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//#define DEBUG
+#ifdef DEBUG
+#define DPRINTF(x) mydprintf x
+static void
+mydprintf(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+#else
+#define DPRINTF(x)
+#endif
+
+/*
+ * Bah, I hate writing on-off-wire conversions in C
+ */
+
+enum { RUMPSP_REQ, RUMPSP_RESP, RUMPSP_ERROR };
+enum {	RUMPSP_SYSCALL,
+	RUMPSP_COPYIN, RUMPSP_COPYINSTR,
+	RUMPSP_COPYOUT, RUMPSP_COPYOUTSTR,
+	RUMPSP_ANONMMAP };
+
+struct rsp_hdr {
+	uint64_t rsp_len;
+	uint64_t rsp_reqno;
+	uint16_t rsp_class;
+	uint16_t rsp_type;
+	/*
+	 * We want this structure 64bit-aligned for typecast fun,
+	 * so might as well use the following for something.
+	 */
+	union {
+		uint32_t sysnum;
+		uint32_t error;
+	} u;
+};
+#define HDRSZ sizeof(struct rsp_hdr)
+#define rsp_sysnum u.sysnum
+#define rsp_error u.error
+
+/*
+ * Data follows the header.  We have two types of structured data.
+ */
+
+/* copyin/copyout */
+struct rsp_copydata {
+	size_t rcp_len;
+	void *rcp_addr;
+	uint8_t rcp_data[0];
+};
+
+/* syscall response */
+struct rsp_sysresp {
+	int rsys_error;
+	register_t rsys_retval[2];
+};
+
+struct respwait {
+	uint64_t rw_reqno;
+	void *rw_data;
+	size_t rw_dlen;
+	int rw_error;
+
+	pthread_cond_t rw_cv;
+
+	TAILQ_ENTRY(respwait) rw_entries;
+};
+
+struct spclient {
+	int spc_fd;
+	int spc_refcnt;
+	int spc_dying;
+
+	pthread_mutex_t spc_mtx;
+	pthread_cond_t spc_cv;
+
+	struct lwp *spc_mainlwp;
+	pid_t spc_pid;
+
+	TAILQ_HEAD(, respwait) spc_respwait;
+
+	/* rest of the fields are zeroed upon disconnect */
+#define SPC_ZEROFF offsetof(struct spclient, spc_pfd)
+	struct pollfd *spc_pfd;
+
+	struct rsp_hdr spc_hdr;
+	uint8_t *spc_buf;
+	size_t spc_off;
+
+	uint64_t spc_nextreq;
+	int spc_ostatus, spc_istatus;
+};
+#define SPCSTATUS_FREE 0
+#define SPCSTATUS_BUSY 1
+#define SPCSTATUS_WANTED 2
+
+typedef int (*addrparse_fn)(const char *, struct sockaddr **, int);
+typedef int (*connecthook_fn)(int);
+
+static int readframe(struct spclient *);
+static void handlereq(struct spclient *);
+
+static __inline void
+spcresetbuf(struct spclient *spc)
+{
+
+	spc->spc_buf = NULL;
+	spc->spc_off = 0;
+}
+
+static __inline void
+spcfreebuf(struct spclient *spc)
+{
+
+	free(spc->spc_buf);
+	spcresetbuf(spc);
+}
+
+static void
+sendlockl(struct spclient *spc)
+{
+
+	/* assert(pthread_mutex_owned) */
+	while (spc->spc_ostatus != SPCSTATUS_FREE) {
+		spc->spc_ostatus = SPCSTATUS_WANTED;
+		pthread_cond_wait(&spc->spc_cv, &spc->spc_mtx);
+	}
+	spc->spc_ostatus = SPCSTATUS_BUSY;
+}
+
+static void
+sendlock(struct spclient *spc)
+{
+
+	pthread_mutex_lock(&spc->spc_mtx);
+	sendlockl(spc);
+	pthread_mutex_unlock(&spc->spc_mtx);
+}
+
+static void
+sendunlockl(struct spclient *spc)
+{
+
+	/* assert(pthread_mutex_owned) */
+	if (spc->spc_ostatus == SPCSTATUS_WANTED)
+		pthread_cond_broadcast(&spc->spc_cv);
+	spc->spc_ostatus = SPCSTATUS_FREE;
+}
+
+static void
+sendunlock(struct spclient *spc)
+{
+
+	pthread_mutex_lock(&spc->spc_mtx);
+	sendunlockl(spc);
+	pthread_mutex_unlock(&spc->spc_mtx);
+}
+
+static int
+dosend(struct spclient *spc, const void *data, size_t dlen)
+{
+	struct pollfd pfd;
+	const uint8_t *sdata = data;
+	ssize_t n;
+	size_t sent;
+	int fd = spc->spc_fd;
+
+	pfd.fd = fd;
+	pfd.events = POLLOUT;
+
+	for (sent = 0, n = 0; sent < dlen; ) {
+		if (n) {
+			if (poll(&pfd, 1, INFTIM) == -1) {
+				if (errno == EINTR)
+					continue;
+				return errno;
+			}
+		}
+
+		n = send(fd, sdata + sent, dlen - sent, MSG_NOSIGNAL);
+		if (n == 0) {
+			return EFAULT;
+		}
+		if (n == -1)  {
+			if (errno != EAGAIN)
+				return EFAULT;
+			continue;
+		}
+		sent += n;
+	}
+
+	return 0;
+}
+
+static void
+putwait(struct spclient *spc, struct respwait *rw, struct rsp_hdr *rhdr)
+{
+
+	rw->rw_data = NULL;
+	rw->rw_dlen = 0;
+	pthread_cond_init(&rw->rw_cv, NULL);
+
+	pthread_mutex_lock(&spc->spc_mtx);
+	rw->rw_reqno = rhdr->rsp_reqno = spc->spc_nextreq++;
+	TAILQ_INSERT_TAIL(&spc->spc_respwait, rw, rw_entries);
+
+	sendlockl(spc);
+}
+
+static void
+unputwait(struct spclient *spc, struct respwait *rw)
+{
+
+	sendunlockl(spc);
+
+	TAILQ_REMOVE(&spc->spc_respwait, rw, rw_entries);
+	pthread_mutex_unlock(&spc->spc_mtx);
+	pthread_cond_destroy(&rw->rw_cv);
+}
+
+static void
+kickwaiter(struct spclient *spc)
+{
+	struct respwait *rw;
+
+	pthread_mutex_lock(&spc->spc_mtx);
+	TAILQ_FOREACH(rw, &spc->spc_respwait, rw_entries) {
+		if (rw->rw_reqno == spc->spc_hdr.rsp_reqno)
+			break;
+	}
+	if (rw == NULL) {
+		DPRINTF(("no waiter found, invalid reqno %" PRIu64 "?\n",
+		    spc->spc_hdr.rsp_reqno));
+		return;
+	}
+	DPRINTF(("rump_sp: client %p woke up waiter at %p\n", spc, rw));
+	rw->rw_data = spc->spc_buf;
+	rw->rw_dlen = (size_t)(spc->spc_off - HDRSZ);
+	if (spc->spc_hdr.rsp_class == RUMPSP_ERROR) {
+		rw->rw_error = spc->spc_hdr.rsp_error;
+	} else {
+		rw->rw_error = 0;
+	}
+	pthread_cond_signal(&rw->rw_cv);
+	pthread_mutex_unlock(&spc->spc_mtx);
+
+	if (rw->rw_error)
+		spcfreebuf(spc);
+	else
+		spcresetbuf(spc);
+}
+
+static void
+kickall(struct spclient *spc)
+{
+	struct respwait *rw;
+
+	/* DIAGASSERT(mutex_owned(spc_lock)) */
+	TAILQ_FOREACH(rw, &spc->spc_respwait, rw_entries)
+		pthread_cond_broadcast(&rw->rw_cv);
+}
+
+static int
+waitresp(struct spclient *spc, struct respwait *rw)
+{
+	struct pollfd pfd;
+	int rv = 0;
+
+	sendunlockl(spc);
+
+	rw->rw_error = 0;
+	while (rw->rw_data == NULL && rw->rw_error == 0 && spc->spc_dying == 0){
+		/* are we free to receive? */
+		if (spc->spc_istatus == SPCSTATUS_FREE) {
+			int gotresp;
+
+			spc->spc_istatus = SPCSTATUS_BUSY;
+			pthread_mutex_unlock(&spc->spc_mtx);
+
+			pfd.fd = spc->spc_fd;
+			pfd.events = POLLIN;
+
+			for (gotresp = 0; !gotresp; ) {
+				switch (readframe(spc)) {
+				case 0:
+					poll(&pfd, 1, INFTIM);
+					continue;
+				case -1:
+					rv = errno;
+					spc->spc_dying = 1;
+					goto cleanup;
+				default:
+					break;
+				}
+
+				switch (spc->spc_hdr.rsp_class) {
+				case RUMPSP_RESP:
+				case RUMPSP_ERROR:
+					kickwaiter(spc);
+					gotresp = spc->spc_hdr.rsp_reqno ==
+					    rw->rw_reqno;
+					break;
+				case RUMPSP_REQ:
+					handlereq(spc);
+					break;
+				default:
+					/* panic */
+					break;
+				}
+			}
+ cleanup:
+			pthread_mutex_lock(&spc->spc_mtx);
+			if (spc->spc_istatus == SPCSTATUS_WANTED)
+				kickall(spc);
+			spc->spc_istatus = SPCSTATUS_FREE;
+		} else {
+			spc->spc_istatus = SPCSTATUS_WANTED;
+			pthread_cond_wait(&rw->rw_cv, &spc->spc_mtx);
+		}
+	}
+
+	TAILQ_REMOVE(&spc->spc_respwait, rw, rw_entries);
+	pthread_mutex_unlock(&spc->spc_mtx);
+
+	pthread_cond_destroy(&rw->rw_cv);
+
+	if (rv)
+		return rv;
+	if (spc->spc_dying)
+		return ENOTCONN;
+	return rw->rw_error;
+}
+
+static int
+readframe(struct spclient *spc)
+{
+	int fd = spc->spc_fd;
+	size_t left;
+	size_t framelen;
+	ssize_t n;
+
+	/* still reading header? */
+	if (spc->spc_off < HDRSZ) {
+		DPRINTF(("rump_sp: readframe getting header at offset %zu\n",
+		    spc->spc_off));
+
+		left = HDRSZ - spc->spc_off;
+		/*LINTED: cast ok */
+		n = read(fd, (uint8_t *)&spc->spc_hdr + spc->spc_off, left);
+		if (n == 0) {
+			return -1;
+		}
+		if (n == -1) {
+			if (errno == EAGAIN)
+				return 0;
+			return -1;
+		}
+
+		spc->spc_off += n;
+		if (spc->spc_off < HDRSZ)
+			return -1;
+
+		/*LINTED*/
+		framelen = spc->spc_hdr.rsp_len;
+
+		if (framelen < HDRSZ) {
+			return -1;
+		} else if (framelen == HDRSZ) {
+			return 1;
+		}
+
+		spc->spc_buf = malloc(framelen - HDRSZ);
+		if (spc->spc_buf == NULL) {
+			return -1;
+		}
+		memset(spc->spc_buf, 0, framelen - HDRSZ);
+
+		/* "fallthrough" */
+	} else {
+		/*LINTED*/
+		framelen = spc->spc_hdr.rsp_len;
+	}
+
+	left = framelen - spc->spc_off;
+
+	DPRINTF(("rump_sp: readframe getting body at offset %zu, left %zu\n",
+	    spc->spc_off, left));
+
+	if (left == 0)
+		return 1;
+	n = read(fd, spc->spc_buf + (spc->spc_off - HDRSZ), left);
+	if (n == 0) {
+		return -1;
+	}
+	if (n == -1) {
+		if (errno == EAGAIN)
+			return 0;
+		return -1;
+	}
+	spc->spc_off += n;
+	left -= n;
+
+	/* got everything? */
+	if (left == 0)
+		return 1;
+	else
+		return 0;
+}
+
+static int
+tcp_parse(const char *addr, struct sockaddr **sa, int allow_wildcard)
+{
+	struct sockaddr_in sin;
+	char buf[64];
+	const char *p;
+	size_t l;
+	int port;
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_len = sizeof(sin);
+	sin.sin_family = AF_INET;
+
+	p = strchr(addr, ':');
+	if (!p) {
+		fprintf(stderr, "rump_sp_tcp: missing port specifier\n");
+		return EINVAL;
+	}
+
+	l = p - addr;
+	if (l > sizeof(buf)-1) {
+		fprintf(stderr, "rump_sp_tcp: address too long\n");
+		return EINVAL;
+	}
+	strncpy(buf, addr, l);
+	buf[l] = '\0';
+
+	/* special INADDR_ANY treatment */
+	if (strcmp(buf, "*") == 0 || strcmp(buf, "0") == 0) {
+		sin.sin_addr.s_addr = INADDR_ANY;
+	} else {
+		switch (inet_pton(AF_INET, buf, &sin.sin_addr)) {
+		case 1:
+			break;
+		case 0:
+			fprintf(stderr, "rump_sp_tcp: cannot parse %s\n", buf);
+			return EINVAL;
+		case -1:
+			fprintf(stderr, "rump_sp_tcp: inet_pton failed\n");
+			return errno;
+		default:
+			assert(/*CONSTCOND*/0);
+			return EINVAL;
+		}
+	}
+
+	if (!allow_wildcard && sin.sin_addr.s_addr == INADDR_ANY) {
+		fprintf(stderr, "rump_sp_tcp: client needs !INADDR_ANY\n");
+		return EINVAL;
+	}
+
+	/* advance to port number & parse */
+	p++;
+	l = strspn(p, "0123456789");
+	if (l == 0) {
+		fprintf(stderr, "rump_sp_tcp: port now found: %s\n", p);
+		return EINVAL;
+	}
+	strncpy(buf, p, l);
+	buf[l] = '\0';
+
+	if (*(p+l) != '/' && *(p+l) != '\0') {
+		fprintf(stderr, "rump_sp_tcp: junk at end of port: %s\n", addr);
+		return EINVAL;
+	}
+
+	port = atoi(buf);
+	if (port < 0 || port >= (1<<(8*sizeof(in_port_t)))) {
+		fprintf(stderr, "rump_sp_tcp: port %d out of range\n", port);
+		return ERANGE;
+	}
+	sin.sin_port = htons(port);
+
+	*sa = malloc(sizeof(sin));
+	if (*sa == NULL)
+		return errno;
+	memcpy(*sa, &sin, sizeof(sin));
+	return 0;
+}
+
+static int
+tcp_connecthook(int s)
+{
+	int x;
+
+	x = 1;
+	setsockopt(s, IPPROTO_TCP, TCP_NODELAY, &x, sizeof(x));
+
+	return 0;
+}
+
+/*ARGSUSED*/
+static int
+unix_parse(const char *addr, struct sockaddr **sa, int allow_wildcard)
+{
+	struct sockaddr_un sun;
+	size_t slen;
+
+	if (strlen(addr) > sizeof(sun.sun_path))
+		return ENAMETOOLONG;
+
+	/*
+	 * The pathname can be all kinds of spaghetti elementals,
+	 * so meek and obidient we accept everything.
+	 */
+	memset(&sun, 0, sizeof(sun));
+	sun.sun_family = AF_LOCAL;
+	strlcpy(sun.sun_path, addr, sizeof(sun.sun_path));
+	sun.sun_len = slen = SUN_LEN(&sun);
+
+	*sa = malloc(slen);
+	if (*sa == NULL)
+		return errno;
+	memcpy(*sa, &sun, slen);
+
+	return 0;
+}
+
+/*ARGSUSED*/
+static int
+notsupp(void)
+{
+
+	fprintf(stderr, "rump_sp: support not yet implemented\n");
+	return EOPNOTSUPP;
+}
+
+static int
+success(void)
+{
+
+	return 0;
+}
+
+struct {
+	const char *id;
+	int domain;
+	addrparse_fn ap;
+	connecthook_fn connhook;
+} parsetab[] = {
+	{ "tcp", PF_INET, tcp_parse, tcp_connecthook },
+	{ "unix", PF_LOCAL, unix_parse, (connecthook_fn)success },
+	{ "tcp6", PF_INET6, (addrparse_fn)notsupp, (connecthook_fn)success },
+};
+#define NPARSE (sizeof(parsetab)/sizeof(parsetab[0]))
+
+static int
+parseurl(const char *url, struct sockaddr **sap, unsigned *idxp,
+	int allow_wildcard)
+{
+	char id[16];
+	const char *p, *p2;
+	size_t l;
+	unsigned i;
+	int error;
+
+	/*
+	 * Parse the url
+	 */
+
+	p = url;
+	p2 = strstr(p, "://");
+	if (!p2) {
+		fprintf(stderr, "rump_sp: invalid locator ``%s''\n", p);
+		return EINVAL;
+	}
+	l = p2-p;
+	if (l > sizeof(id)-1) {
+		fprintf(stderr, "rump_sp: identifier too long in ``%s''\n", p);
+		return EINVAL;
+	}
+
+	strncpy(id, p, l);
+	id[l] = '\0';
+	p2 += 3; /* beginning of address */
+
+	for (i = 0; i < NPARSE; i++) {
+		if (strcmp(id, parsetab[i].id) == 0) {
+			error = parsetab[i].ap(p2, sap, allow_wildcard);
+			if (error)
+				return error;
+			break;
+		}
+	}
+	if (i == NPARSE) {
+		fprintf(stderr, "rump_sp: invalid identifier ``%s''\n", p);
+		return EINVAL;
+	}
+
+	*idxp = i;
+	return 0;
+}
