a029881ccef0bd6a01d50a15f2ab9d87f77386f6
joerg <joerg>
2011-03-09 23:10:05 +0000

    Add TLS support infrastructure. For dynamic binaries, ld.elf_so exports _rtld_tls_allocate and _rtld_tls_free. libpthread uses this functions to setup the thread private area of all new threads. ld.elf_so is responsible for setting up the private area for the initial thread. Similar functions are called from _libc_init for static binaries, using dl_iterate_phdr to access the ELF Program Header.
    Add test cases to exercise the different TLS storage models. Test cases
    are compiled and installed on all platforms, but are skipped on
    platforms not marked for TLS support.
    
    This material is based upon work partially supported by
    The NetBSD Foundation under a contract with Joerg Sonnenberger.
    
    It is inspired by the TLS support in FreeBSD by Doug Rabson and the
    clean ups of the DragonFly port of the original FreeBSD modifications.

--- sys/arch/arc/include/netbsd32_machdep.h
+++
@@ -1,3 +0,0 @@
-/* $NetBSD: netbsd32_machdep.h,v 1.1.8.1 2011/03/05 15:09:31 bouyer Exp $ */
-
-#include <mips/netbsd32_machdep.h>
--- sys/arch/sbmips/include/types.h
+++
@@ -1,7 +0,0 @@
-/* $NetBSD: types.h,v 1.7.40.1 2011/03/05 15:10:00 bouyer Exp $ */
-
-#define	_MIPS_PADDR_T_64BIT
-
-#include <mips/types.h>
-
-#define	__HAVE_DEVICE_REGISTER
