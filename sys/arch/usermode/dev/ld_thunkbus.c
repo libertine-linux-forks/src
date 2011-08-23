6a1ae8abbad31495b0329a4f1aafd4be1c3c3d4d
jmcneill <jmcneill>
2011-08-23 17:12:32 +0000

    struct stat also has time_t in it, so we can't use that in the thunk api either

---
+++ sys/arch/usermode/dev/ld_thunkbus.c
@@ -0,0 +1,233 @@
+/* $NetBSD: ld_thunkbus.c,v 1.5 2011/08/23 17:12:32 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2011 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: ld_thunkbus.c,v 1.5 2011/08/23 17:12:32 jmcneill Exp $");
+
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/buf.h>
+#include <sys/disk.h>
+#include <sys/kmem.h>
+
+#include <dev/ldvar.h>
+
+#include <machine/mainbus.h>
+#include <machine/thunk.h>
+
+static int	ld_thunkbus_match(device_t, cfdata_t, void *);
+static void	ld_thunkbus_attach(device_t, device_t, void *);
+
+static int	ld_thunkbus_ldstart(struct ld_softc *, struct buf *);
+static int	ld_thunkbus_lddump(struct ld_softc *, void *, int, int);
+static int	ld_thunkbus_ldflush(struct ld_softc *, int);
+
+static void	ld_thunkbus_sig(int, siginfo_t *, void *);
+static void	ld_thunkbus_complete(void *);
+
+struct ld_thunkbus_softc;
+
+struct ld_thunkbus_transfer {
+	struct ld_thunkbus_softc *tt_sc;
+	struct aiocb	tt_aio;
+	struct buf	*tt_bp;
+};
+
+struct ld_thunkbus_softc {
+	struct ld_softc	sc_ld;
+
+	int		sc_fd;
+	void		*sc_ih;
+
+	struct ld_thunkbus_transfer sc_tt;
+};
+
+CFATTACH_DECL_NEW(ld_thunkbus, sizeof(struct ld_thunkbus_softc),
+    ld_thunkbus_match, ld_thunkbus_attach, NULL, NULL);
+
+extern int errno;
+
+static int
+ld_thunkbus_match(device_t parent, cfdata_t match, void *opaque)
+{
+	struct thunkbus_attach_args *taa = opaque;
+
+	if (taa->taa_type != THUNKBUS_TYPE_DISKIMAGE)
+		return 0;
+
+	return 1;
+}
+
+static void
+ld_thunkbus_attach(device_t parent, device_t self, void *opaque)
+{
+	struct ld_thunkbus_softc *sc = device_private(self);
+	struct ld_softc *ld = &sc->sc_ld;
+	struct thunkbus_attach_args *taa = opaque;
+	const char *path = taa->u.diskimage.path;
+	struct sigaction sa;
+	ssize_t size, blksize;
+
+	ld->sc_dv = self;
+
+	sc->sc_fd = thunk_open(path, O_RDWR, 0);
+	if (sc->sc_fd == -1) {
+		aprint_error(": couldn't open %s: %d\n", path, errno);
+		return;
+	}
+	if (thunk_fstat_getsize(sc->sc_fd, &size, &blksize) == -1) {
+		aprint_error(": couldn't stat %s: %d\n", path, errno);
+		return;
+	}
+
+	aprint_naive("\n");
+	aprint_normal(": %s (%lld)\n", path, (long long)size);
+
+	ld->sc_flags = LDF_ENABLED;
+	ld->sc_maxxfer = blksize;
+	ld->sc_secsize = 512;
+	ld->sc_secperunit = size / ld->sc_secsize;
+	ld->sc_maxqueuecnt = 1;
+	ld->sc_start = ld_thunkbus_ldstart;
+	ld->sc_dump = ld_thunkbus_lddump;
+	ld->sc_flush = ld_thunkbus_ldflush;
+
+	sc->sc_ih = softint_establish(SOFTINT_BIO,
+	    ld_thunkbus_complete, sc);
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART|SA_SIGINFO;
+	sa.sa_sigaction = ld_thunkbus_sig;
+	if (thunk_sigaction(SIGIO, &sa, NULL) == -1)
+		panic("couldn't register SIGIO handler: %d", errno);
+
+	ldattach(ld);
+}
+
+static void
+ld_thunkbus_sig(int sig, siginfo_t *info, void *ctx)
+{
+	struct ld_thunkbus_transfer *tt;
+	struct ld_thunkbus_softc *sc;
+
+	curcpu()->ci_idepth++;
+
+	if (info->si_signo == SIGIO) {
+		tt = info->si_value.sival_ptr;
+		sc = tt->tt_sc;
+		softint_schedule(sc->sc_ih);
+	}
+
+	curcpu()->ci_idepth--;
+}
+
+static void
+ld_thunkbus_complete(void *arg)
+{
+	struct ld_thunkbus_softc *sc = arg;
+	struct ld_thunkbus_transfer *tt = &sc->sc_tt;
+	struct buf *bp = tt->tt_bp;
+
+	if (thunk_aio_error(&tt->tt_aio) == 0 &&
+	    thunk_aio_return(&tt->tt_aio) != -1) {
+		bp->b_resid = 0;
+	} else {
+		bp->b_error = errno;
+		bp->b_resid = bp->b_bcount;
+	}
+
+	if (bp->b_error)
+		printf("errpr!\n");
+
+	lddone(&sc->sc_ld, bp);
+}
+
+static int
+ld_thunkbus_ldstart(struct ld_softc *ld, struct buf *bp)
+{
+	struct ld_thunkbus_softc *sc = (struct ld_thunkbus_softc *)ld;
+	struct ld_thunkbus_transfer *tt = &sc->sc_tt;
+	int error;
+
+	tt->tt_sc = sc;
+	tt->tt_bp = bp;
+	memset(&tt->tt_aio, 0, sizeof(tt->tt_aio));
+	tt->tt_aio.aio_fildes = sc->sc_fd;
+	tt->tt_aio.aio_buf = bp->b_data;
+	tt->tt_aio.aio_nbytes = bp->b_bcount;
+	tt->tt_aio.aio_offset = bp->b_rawblkno * ld->sc_secsize;
+
+	tt->tt_aio.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+	tt->tt_aio.aio_sigevent.sigev_signo = SIGIO;
+	tt->tt_aio.aio_sigevent.sigev_value.sival_ptr = tt;
+
+#if 0
+	device_printf(sc->sc_ld.sc_dv, "%s off=%lld count=%lld\n",
+	    (bp->b_flags & B_READ) ? "rd" : "wr",
+	    (long long)bp->b_rawblkno,
+	    (long long)bp->b_bcount);
+#endif
+
+	if (bp->b_flags & B_READ)
+		error = thunk_aio_read(&tt->tt_aio);
+	else
+		error = thunk_aio_write(&tt->tt_aio);
+
+	return error == -1 ? errno : 0;
+}
+
+static int
+ld_thunkbus_lddump(struct ld_softc *ld, void *data, int blkno, int blkcnt)
+{
+	struct ld_thunkbus_softc *sc = (struct ld_thunkbus_softc *)ld;
+	ssize_t len;
+
+	len = thunk_pwrite(sc->sc_fd, data, blkcnt, blkno);
+	if (len == -1)
+		return errno;
+	else if (len != blkcnt) {
+		device_printf(ld->sc_dv, "%s failed (short xfer)\n", __func__);
+		return EIO;
+	}
+
+	return 0;
+}
+
+static int
+ld_thunkbus_ldflush(struct ld_softc *ld, int flags)
+{
+	struct ld_thunkbus_softc *sc = (struct ld_thunkbus_softc *)ld;
+
+	if (thunk_fsync(sc->sc_fd) == -1)
+		return errno;
+
+	return 0;
+}
+
---
+++ sys/arch/usermode/usermode/thunk.c
@@ -0,0 +1,308 @@
+/* $NetBSD: thunk.c,v 1.15 2011/08/23 17:12:32 jmcneill Exp $ */
+
+/*-
+ * Copyright (c) 2011 Jared D. McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__RCSID("$NetBSD: thunk.c,v 1.15 2011/08/23 17:12:32 jmcneill Exp $");
+
+#include <sys/types.h>
+#include <sys/ansi.h>
+
+#include <aio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <ucontext.h>
+#include <unistd.h>
+
+#include "../include/thunk.h"
+
+static void
+thunk_to_timeval(const struct thunk_timeval *ttv, struct timeval *tv)
+{
+	tv->tv_sec = ttv->tv_sec;
+	tv->tv_usec = ttv->tv_usec;
+}
+
+static void
+thunk_from_timeval(const struct timeval *tv, struct thunk_timeval *ttv)
+{
+	ttv->tv_sec = tv->tv_sec;
+	ttv->tv_usec = tv->tv_usec;
+}
+
+static void
+thunk_to_itimerval(const struct thunk_itimerval *tit, struct itimerval *it)
+{
+	thunk_to_timeval(&tit->it_interval, &it->it_interval);
+	thunk_to_timeval(&tit->it_value, &it->it_value);
+}
+
+static void
+thunk_from_itimerval(const struct itimerval *it, struct thunk_itimerval *tit)
+{
+	thunk_from_timeval(&it->it_interval, &tit->it_interval);
+	thunk_from_timeval(&it->it_value, &tit->it_value);
+}
+
+int
+thunk_setitimer(int which, const struct thunk_itimerval *value,
+    struct thunk_itimerval *ovalue)
+{
+	struct itimerval it, oit;
+	int error;
+
+	thunk_to_itimerval(value, &it);
+	error = setitimer(which, &it, &oit);
+	if (error)
+		return error;
+	if (ovalue)
+		thunk_from_itimerval(&oit, ovalue);
+
+	return 0;
+}
+
+int
+thunk_gettimeofday(struct thunk_timeval *tp, void *tzp)
+{
+	struct timeval tv;
+	int error;
+
+	error = gettimeofday(&tv, tzp);
+	if (error)
+		return error;
+
+	thunk_from_timeval(&tv, tp);
+
+	return 0;
+}
+
+unsigned int
+thunk_getcounter(void)
+{
+	struct timespec ts;
+	int error;
+
+	error = clock_gettime(CLOCK_MONOTONIC, &ts);
+	if (error) {
+		perror("clock_gettime CLOCK_MONOTONIC");
+		abort();
+	}
+
+	return (unsigned int)(ts.tv_sec * 1000000000ULL + ts.tv_nsec);
+}
+
+long
+thunk_clock_getres_monotonic(void)
+{
+	struct timespec res;
+	int error;
+
+	error = clock_getres(CLOCK_MONOTONIC, &res);
+	if (error)
+		return -1;
+
+	return res.tv_nsec;
+}
+
+int
+thunk_usleep(useconds_t microseconds)
+{
+	return usleep(microseconds);
+}
+
+void
+thunk_exit(int status)
+{
+	return exit(status);
+}
+
+void
+thunk_abort(void)
+{
+	abort();
+}
+
+int
+thunk_getcontext(ucontext_t *ucp)
+{
+	return getcontext(ucp);
+}
+
+int
+thunk_setcontext(const ucontext_t *ucp)
+{
+	return setcontext(ucp);
+}
+
+void
+thunk_makecontext(ucontext_t *ucp, void (*func)(), int argc,
+    void (*arg1)(void *), void *arg2)
+{
+	assert(argc == 2);
+
+	makecontext(ucp, func, argc, arg1, arg2);
+}
+
+int
+thunk_swapcontext(ucontext_t *oucp, ucontext_t *ucp)
+{
+	return swapcontext(oucp, ucp);
+}
+
+int
+thunk_getchar(void)
+{
+	return getchar();
+}
+
+void
+thunk_putchar(int c)
+{
+	putchar(c);
+	fflush(stdout);
+}
+
+int
+thunk_execv(const char *path, char * const argv[])
+{
+	return execv(path, argv);
+}
+
+int
+thunk_open(const char *path, int flags, mode_t mode)
+{
+	return open(path, flags, mode);
+}
+
+int
+thunk_fstat_getsize(int fd, ssize_t *size, ssize_t *blksize)
+{
+	struct stat st;
+	int error;
+
+	error = fstat(fd, &st);
+	if (error)
+		return -1;
+
+	if (size)
+		*size = st.st_size;
+	if (blksize)
+		*blksize = st.st_blksize;
+
+	return 0;
+}
+
+ssize_t
+thunk_pread(int d, void *buf, size_t nbytes, off_t offset)
+{
+	return pread(d, buf, nbytes, offset);
+}
+
+ssize_t
+thunk_pwrite(int d, const void *buf, size_t nbytes, off_t offset)
+{
+	return pwrite(d, buf, nbytes, offset);
+}
+
+int
+thunk_fsync(int fd)
+{
+	return fsync(fd);
+}
+
+int
+thunk_mkstemp(char *template)
+{
+	return mkstemp(template);
+}
+
+int
+thunk_unlink(const char *path)
+{
+	return unlink(path);
+}
+
+int
+thunk_sigaction(int sig, const struct sigaction *act, struct sigaction *oact)
+{
+	return sigaction(sig, act, oact);
+}
+
+int
+thunk_aio_read(struct aiocb *aiocbp)
+{
+	return aio_read(aiocbp);
+}
+
+int
+thunk_aio_write(struct aiocb *aiocbp)
+{
+	return aio_write(aiocbp);
+}
+
+int
+thunk_aio_error(const struct aiocb *aiocbp)
+{
+	return aio_error(aiocbp);
+}
+
+int
+thunk_aio_return(struct aiocb *aiocbp)
+{
+	return aio_return(aiocbp);
+}
+
+void *
+thunk_sbrk(intptr_t len)
+{
+	return sbrk(len);
+}
+
+void *
+thunk_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+	return mmap(addr, len, prot, flags, fd, offset);
+}
+
+int
+thunk_munmap(void *addr, size_t len)
+{
+	return munmap(addr, len);
+}
+
+int
+thunk_mprotect(void *addr, size_t len, int prot)
+{
+	return mprotect(addr, len, prot);
+}
+
